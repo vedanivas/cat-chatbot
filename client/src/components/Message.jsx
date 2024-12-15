602c488978156d0177631e1eafb764ec33562102
vedanivas <veda.nivas@students.iiit.ac.in>
2024-12-16 01:04:11 +0400

    remove wrong frontend

--- client/src/components/Message.jsx
+++
@@ -1,9 +0,0 @@
-const Message = ({ sender, content }) => {
-  return (
-    <div className={`message ${sender}`}>
-      <div className="message-content">{content}</div>
-    </div>
-  );
-};
-
-export default Message;
